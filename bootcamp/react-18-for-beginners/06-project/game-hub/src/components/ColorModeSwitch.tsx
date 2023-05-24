99cb0a6e418ddf22ad7d43e3536187b6cfe66319
rohansachdeva1990 <rohan.sachdeva1990@gmail.com>
2023-05-24 21:39:42 +0200

    [game-hub] build search input

--- bootcamp/react-18-for-beginners/06-project/game-hub/src/components/ColorModeSwitch.tsx
+++ bootcamp/react-18-for-beginners/06-project/game-hub/src/components/ColorModeSwitch.tsx
@@ -1,16 +1,12 @@
-import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+import { HStack, Switch, Text, useColorMode } from '@chakra-ui/react';
 
 const ColorModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode();
 
   return (
     <HStack>
-      <Switch
-        colorScheme="green"
-        isChecked={colorMode === "dark"}
-        onChange={toggleColorMode}
-      />
-      <Text>Dark Mode</Text>
+      <Switch colorScheme='green' isChecked={colorMode === 'dark'} onChange={toggleColorMode} />
+      <Text whiteSpace={'nowrap'}>Dark Mode</Text>
     </HStack>
   );
 };
