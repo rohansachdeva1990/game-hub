a683c3ce124059df09390c05369ee4577a2e8db2
rohansachdeva1990 <rohan.sachdeva1990@gmail.com>
2023-05-24 21:49:13 +0200

    [game-hub] implement searching

--- bootcamp/react-18-for-beginners/06-project/game-hub/src/components/SearchInput.tsx
+++ bootcamp/react-18-for-beginners/06-project/game-hub/src/components/SearchInput.tsx
@@ -1,12 +1,28 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
 import { BsSearch } from 'react-icons/bs';
+import { FormEvent, useRef } from 'react';
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    if (ref.current) {
+      onSearch(ref.current.value);
+    }
+  };
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder='Search games...' variant='filled' />;
-    </InputGroup>
+    <form onSubmit={handleSubmit}>
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <Input ref={ref} borderRadius={20} placeholder='Search games...' variant='filled' />;
+      </InputGroup>
+    </form>
   );
 };
 
